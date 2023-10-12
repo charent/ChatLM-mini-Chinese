f4e8cdbd0312c8ce16815e261478af57ab537592
charent <19562666+charent@users.noreply.github.com>
2023-10-12 20:14:05 +0800

    add infer

---
+++ train.py
@@ -0,0 +1,15 @@
+import fire
+
+from config import  TrainConfig, T5ModelConfig
+from model.trainer import ChatTrainer
+
+
+if __name__ == '__main__':
+    train_config = TrainConfig()
+    model_config = T5ModelConfig()
+
+    chat_trainer = ChatTrainer(train_config=train_config, model_config=model_config)
+
+    # 解析命令行参数，执行指定函数
+    # e.g: python main.py train
+    fire.Fire(component=chat_trainer)
